8cf5c933c49e5bf592ca65642b91d2c407953e10
onePeterCollins <onepetercollins@gmail.com>
2020-05-04 22:12:53 +0400

    updated and responsive

--- precache-manifest.5b4efdb1337441d05979234ac827d05c.js
+++ precache-manifest.5b4efdb1337441d05979234ac827d05c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7d940774e3a56764f696c8dec6908a92",
+    "revision": "0888dd61f4ea2cb3262a1fcd9966d2cb",
     "url": "/index.html"
   },
   {
-    "revision": "b4c8a830ff0e4e6359bb",
-    "url": "/static/css/main.1ac7af28.chunk.css"
+    "revision": "b109c3d66f8f8ce39017",
+    "url": "/static/css/main.eef86cd6.chunk.css"
   },
   {
     "revision": "c3ca98c4c4be02c0b294",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.67df4870.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b4c8a830ff0e4e6359bb",
-    "url": "/static/js/main.238c7a7b.chunk.js"
+    "revision": "b109c3d66f8f8ce39017",
+    "url": "/static/js/main.78fff0d9.chunk.js"
   },
   {
     "revision": "b02cfc66de1c778d6295",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d8af283a40673357e2cb1d40d78b6c7.js"
+  "/precache-manifest.5b4efdb1337441d05979234ac827d05c.js"
 );
 
 self.addEventListener('message', (event) => {
