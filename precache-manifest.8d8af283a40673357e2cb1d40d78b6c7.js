cf097b60967aff975bab876ab8176a0001a10d1f
chimdipetercollins <cpnzereogu@gmail.com>
2020-05-04 18:13:21 +0400

    initial commit and deploy

---
+++ precache-manifest.8d8af283a40673357e2cb1d40d78b6c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d940774e3a56764f696c8dec6908a92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4c8a830ff0e4e6359bb",
+    "url": "/static/css/main.1ac7af28.chunk.css"
+  },
+  {
+    "revision": "c3ca98c4c4be02c0b294",
+    "url": "/static/js/2.67df4870.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.67df4870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c8a830ff0e4e6359bb",
+    "url": "/static/js/main.238c7a7b.chunk.js"
+  },
+  {
+    "revision": "b02cfc66de1c778d6295",
+    "url": "/static/js/runtime-main.bc71534c.js"
+  },
+  {
+    "revision": "c7caff6be13b3499c1de747770cf4120",
+    "url": "/static/media/aim.c7caff6b.png"
+  },
+  {
+    "revision": "4fc432214a36e952d864e530ff75d8f6",
+    "url": "/static/media/target.4fc43221.png"
+  }
+]);
